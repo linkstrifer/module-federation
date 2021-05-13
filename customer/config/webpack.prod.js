99273567fd1a026bf670f6ba0b1482f414f2a2cc
Link Strifer <linkstrifer@outlook.com>
2021-05-12 22:39:24 -0500

    add javascript project

---
+++ customer/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+const commonConfig = require("./webpack.common");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/customer/latest/",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "customer",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./app": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
---
+++ customer/src/bootstrap.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<div>Customer Route</div>, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_consumer-development-root");
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
